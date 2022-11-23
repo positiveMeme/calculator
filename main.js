68a1a88138ccf9781e3670697ca477933c2f99ea
positiveMeme <joe.arthurs@hotmail.co.uk>
2022-11-22 21:33:36 -0500

    html done, basic functions added, main css done. Yet to add colour changes on click

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('hello');
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(func, a, b) {
+    return func(a, b);
+}
